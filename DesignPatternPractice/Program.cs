4913ac81d8107119bd4de2d0b235f789458eceef
Matt Tomlinson <mtomlinson@navexglobal.com>
2022-09-13 11:32:14 -0600

    better input experience

--- DesignPatternPractice/Program.cs
+++ DesignPatternPractice/Program.cs
@@ -3,20 +3,27 @@ using DesignPatternPractice.Abstract_Factory;
 using DesignPatternPractice.Builder;
 
 //https://www.dofactory.com/net/design-patterns
-bool exit = false;
 
-while (!exit) {
+List<object> patterns = new List<object>() { 
+	new AbstractFactory(),
+	new Builder()
+};
+
+while (true) {
+	Console.WriteLine("Select a pattern to run:");
+    for (int i = 0; i < patterns.Count; i++) {
+		Console.WriteLine($" {i} - {patterns[i].GetType().Name}");
+    }
+
+	char c = Console.ReadKey().KeyChar;
 	Console.Clear();
 
-	DesignPattern currentDesignPattern = new Builder();
-	currentDesignPattern.Run();
-
-	try {
-		char c = Console.ReadKey().KeyChar;
-		if (c == 'q') {
-			exit = true;
-		}
-	} catch {
+	if (int.TryParse(c.ToString(), out int index)) {
+		DesignPattern? currentDesignPattern = patterns[index] as DesignPattern;
+		currentDesignPattern?.Run();
+	} else if (c == 'q') {
 		Environment.Exit(0);
 	}
+
+	Console.WriteLine();
 }
